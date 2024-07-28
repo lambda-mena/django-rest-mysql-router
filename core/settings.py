29477d28a3f06d38feb9758b3ceb637e624018a7
lambda-mena <juanmena1605@gmail.com>
2024-07-28 15:25:16 -0500

    settings: add deploy env variables

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gr@$^fp1cxif33k$ten37na$rj18&cf_&w+f%yj@&3i5ll717("
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(config("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config("HOSTNAME")]
 
 
 # Application definition
