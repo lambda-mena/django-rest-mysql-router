9b837abeffdfb19d4613de5d4ba016c9345e1005
lambda-mena <juan.cuevas.mena@gmail.com>
2024-10-26 18:07:13 -0500

    feat: add two database and django router

--- core/settings.py
+++ core/settings.py
@@ -96,16 +96,32 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "default": {},
+    "write": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_WRITE_PORT"),
+    },
+    "read": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_READ_PORT"),
     }
 }
 
+DATABASE_ROUTERS = ["database.router.DatabaseRouter"]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
